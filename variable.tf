e740e36ca3f859fc0c37aacfe86c29ffe9dfd497
Renukota <renumadhuri.26@gmail.com>
2026-02-06 16:13:24 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  description = "name of the server"
+  type        = string
+  default     = "devsever"
+}
+
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "client" {
+  type    = string
+  default = "TCS"
+}
+
+variable "amid" {
+  type    = string
+  default = "ami-03ea746da1a2e36e7"
+}
+
+variable "type" {
+  type    = string
+  default = "c7i-flex.large"
+}
+
+variable "keyname" {
+  type    = string
+  default = "secondKey-pair-ohio"
+}
+
+variable "size" {
+  type    = number
+  default = "10"
+}
