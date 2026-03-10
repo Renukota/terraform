cc09e90f1a634e93e15d515c0430b25be21f17cc
Renukota <renumadhuri.26@gmail.com>
2026-03-10 16:08:02 +0530

    Set count to 2 for instance creation

--- index_main.tf
+++ index_main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "myServer" {
 ami           = var.amid[count.index]
   instance_type = var.type[count.index]
   key_name      = var.keyname
+ count =2
   root_block_device {
     volume_size = var.size[count.index]
   }
